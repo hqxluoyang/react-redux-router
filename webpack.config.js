7537d6f530cfc3744b0fe46ba2355493c002ce00
houqingxin <houqingxin@shanchuan.cn>
2016-05-19 13:42:33 +0800

    git merge

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,9 @@ module.exports = {
       include: __dirname
     },{
       test:/\.css$/,
-      loader:'style!css'
+     // loader:'style!css'
+     loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+
     },{
       test:/\.less$/,
       loader:'style!css!less'
